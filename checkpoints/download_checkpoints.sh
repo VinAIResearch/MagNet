9da579953204d0f6b037369cd426c32ae7b4c059
Chuong Huynh <minhchuong.itus@gmail.com>
2021-04-02 09:48:47 +0700

    [UPDATE] gleason checkpoints

--- checkpoints/download_checkpoints.sh
+++ checkpoints/download_checkpoints.sh
@@ -1,3 +1,5 @@
 # For deepglobe dataset
 wget https://public.vinai.io/magnet_cvpr2021/deepglobe_fpn.pth
 wget https://public.vinai.io/magnet_cvpr2021/deepglobe_refinement.pth
+wget https://public.vinai.io/magnet_cvpr2021/gleason_psp.pth
+wget https://public.vinai.io/magnet_cvpr2021/gleason_refinement.pth
